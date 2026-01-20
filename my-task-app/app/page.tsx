d337fa06d6c515d2fab154101e551cab0186c224
DartaKraulina <dartakraulina@gmail.com>
2026-01-20 11:30:31 +0200

    projekts ir izveidots

---
+++ my-task-app/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Task List App
+      </h1>
+      <p>SSR un Tailwind ir uzstādīti!</p>
+    </main>
+  );
+}
